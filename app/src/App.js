952134c5cd57ac514274813c2ee066d4cada43e1
Matheus Clemente <matheus_klementt@hotmail.com>
2019-04-27 17:56:42 -0300

    Testing screen
    Co-authored-by: João Victor <joao.bisinotti@candev.com.br>

--- app/src/App.js
+++ app/src/App.js
@@ -1,17 +1,20 @@
-import React, { Component } from "react";
-import { Container, Header, Content, Accordion } from "native-base";
-const dataArray = [
-  { title: "First Element", content: "Lorem ipsum dolor sit amet" },
-  { title: "Second Element", content: "Lorem ipsum dolor sit amet" },
-  { title: "Third Element", content: "Lorem ipsum dolor sit amet" }
-];
-export default class AccordionExample extends Component {
+import React, { Component } from 'react';
+import { Container, Content, InputGroup, Input } from 'native-base';
+import { Button, Text } from 'native-base';
+
+export default class FormExample extends Component {
   render() {
     return (
       <Container>
-        <Header />
-        <Content padder>
-          <Accordion dataArray={dataArray} expanded={0}/>
+        
+        <Content>
+         
+          <InputGroup >
+            <Input inlineLabel label='NAME' placeholder='Nome da Sala' />
+          </InputGroup>
+  
+          <Button><Text>Criar!</Text></Button>
+
         </Content>
       </Container>
     );
