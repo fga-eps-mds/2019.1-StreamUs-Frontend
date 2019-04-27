b43fca01bea44ebec04632fe213ee468e30485e9
Ygor Galeno <ygor.tgaleno@gmail.com>
2019-04-27 16:10:30 -0300

    Adding docker

---
+++ app/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import { Container, Header, Content, Accordion } from "native-base";
+const dataArray = [
+  { title: "First Element", content: "Lorem ipsum dolor sit amet" },
+  { title: "Second Element", content: "Lorem ipsum dolor sit amet" },
+  { title: "Third Element", content: "Lorem ipsum dolor sit amet" }
+];
+export default class AccordionExample extends Component {
+  render() {
+    return (
+      <Container>
+        <Header />
+        <Content padder>
+          <Accordion dataArray={dataArray} expanded={0}/>
+        </Content>
+      </Container>
+    );
+  }
+}
