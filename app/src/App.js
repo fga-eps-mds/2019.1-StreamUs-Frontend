7ff6fb20fcaf95e4f0ab9ed381f50f8b8ad66493
Matheus Clemente <matheus_klementt@hotmail.com>
2019-05-02 20:50:22 -0300

    Applying button and background colors
    Co-authored-by: João Victor <joao.bisinotti@candev.com.br>

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Container, Content, InputGroup, Input, ListItem } from 'native-base';
+import { View, InputGroup, Input, List, ListItem } from 'native-base';
 import { Button, Text } from 'native-base';
 import { CheckBox, Body } from 'native-base';
 import {StyleSheet, Dimensions} from 'react-native';
@@ -7,16 +7,22 @@ import {StyleSheet, Dimensions} from 'react-native';
 const {width: screenWidth, heigth: screenHeigth} = Dimensions.get('window')
 
 const styles = StyleSheet.create({
-  container: {
+  view: {
+    heigth: screenHeigth,
+    flex: 1,
+    flexDirection: 'row',
+    alignItems:'center',
+    justifyContent:'center',
+    backgroundColor: '#191414'
+    },
+  list: {
     width: screenWidth/2,
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems:'center'
   },
+
   button: {
     width: screenWidth/5,
-    alignItems:'center',
-    justifyContent: 'center'
+    backgroundColor: '#1DB954'
+
   },
 })
 
@@ -31,30 +37,29 @@ export default class FormExample extends Component {
   }
   render() {
     return (
-      <Container style={styles.container}>
-        
-        <Content>
+      <View style={styles.view}>
+          <List style={styles.list}>
            <ListItem>
             <InputGroup >
               <Input inlineLabel label='NAME' placeholder='Nome da Sala' onChange={(event) => this.setText(event, 'nameRoom')}/>
             </InputGroup>
-          </ListItem>
+            </ListItem>
 
-          <ListItem>
-            <CheckBox
-            key={'streamId'} 
-            checked={this.state.checked.streamId} 
-            onPress={() => this.setState({checked: {streamId: !this.state.checked.streamId}})} 
-            />
-              <Body>
-                <Text>Permitir a entrada de membros por StreamId</Text>
-              </Body>
-          </ListItem>
+            <ListItem>
+              <CheckBox
+              color = '#1DB954'
+              key={'streamId'} 
+              checked={this.state.checked.streamId} 
+              onPress={() => this.setState({checked: {streamId: !this.state.checked.streamId}})} 
+              />
+                <Body>
+                  <Text>Permitir a entrada de membros por StreamId</Text>
+                </Body>
+            </ListItem>
           <Text>{this.state.nameRoom}</Text>
           <Button style={styles.button}><Text>Criar!</Text></Button>
-
-        </Content>
-      </Container>
+          </List>
+      </View>
     );
   }
 }
