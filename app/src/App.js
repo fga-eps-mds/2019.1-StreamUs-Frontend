2dd39878146f1251f772faaf5d580908d286202d
Lucas Lopes Pereira <44032349+LucasPLopes@users.noreply.github.com>
2019-05-08 16:34:21 -0300

    Update App.js
    #7 
    Begin of Issue
    
    
    
    Co-authored-by: matheusclemente<matheusclemente@users.noreply.github.com>

--- app/src/App.js
+++ app/src/App.js
@@ -1,19 +1,10 @@
 import React, { Component } from "react";
-import { Container, Header, Content, Accordion } from "native-base";
-const dataArray = [
-  { title: "First Element", content: "Lorem ipsum dolor sit amet" },
-  { title: "Second Element", content: "Lorem ipsum dolor sit amet" },
-  { title: "Third Element", content: "Lorem ipsum dolor sit amet" }
-];
+import {  } from "native-base";
+
 export default class AccordionExample extends Component {
   render() {
     return (
-      <Container>
-        <Header />
-        <Content padder>
-          <Accordion dataArray={dataArray} expanded={0}/>
-        </Content>
-      </Container>
+      
     );
   }
 }
