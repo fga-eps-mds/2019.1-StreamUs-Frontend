9f180face78d249559800725d80856dad1e19771
Ygor Galeno <ygor.tgaleno@gmail.com>
2019-05-09 21:12:14 -0300

    Hot fix

--- app/src/App.js
+++ app/src/App.js
@@ -1,17 +1,19 @@
-import React, { Component } from "react";
-import { Container, Header, Content, Accordion } from "native-base";
-const dataArray = [
-  { title: "First Element", content: "Lorem ipsum dolor sit amet" },
-  { title: "Second Element", content: "Lorem ipsum dolor sit amet" },
-  { title: "Third Element", content: "Lorem ipsum dolor sit amet" }
-];
-export default class AccordionExample extends Component {
+import React, { Component } from 'react';
+import { Container, Header, Content, Form, Item, Input } from 'native-base';
+export default class FormExample extends Component {
   render() {
     return (
       <Container>
         <Header />
-        <Content padder>
-          <Accordion dataArray={dataArray} expanded={0}/>
+        <Content>
+          <Form>
+            <Item>
+              <Input placeholder="Username" />
+            </Item>
+            <Item last>
+              <Input placeholder="Password" />
+            </Item>
+          </Form>
         </Content>
       </Container>
     );
