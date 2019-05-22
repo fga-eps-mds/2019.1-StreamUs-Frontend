08ef8882f953bc889c6133f65e8777166a30fa0f
iagoomr <iagorag363@gmail.com>
2019-05-21 20:50:27 -0500

    Create property deleteTrack issue #15
    Co-authored-by: pdaniel37 <pedrodaniel@candev.com.br>
    Co-authored-by: oliveiraMarcelo <martins.oliveira.mo@gmail.com>
    Co-authored-by: Matheus Clemente <matheus_klementt@hotmail.com>

--- app/src/ManagePlaylist.js
+++ app/src/ManagePlaylist.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Container, Header, Item, Input, Icon, Button, Text} from 'native-base';
+import { Container, Header, Item, Input, Icon, Button, Text } from 'native-base';
 
 
 
@@ -10,22 +10,41 @@ export default class ManagePlaylist extends Component {
         fetch(url, {
             method: "POST",
             headers: {
-                Authorization: "BQC6GgNxI6gYT7FvNLDNYWm7AZzqBThwQAuANGgbGnqCgzFY6b_r-bDcJf3OEA8tGYB9Xs3HQla5d3OUaVTzhtHQq4iV-ne6-PYZr6Gzng_M5LFO60PTA619SZjb34j6P6TlNaC_hQM6lvp3Kh0DpI2ydavPuM5V",
+                Authorization: "authorization/json",
                 "Content-Type": "application/json"
             },
-            body: JSON.stringify ({
+            body: JSON.stringify({
                 "uris": `spotify:track:${track_id}`
             }),
         })
-        .then(response => {
-            response.json()
-            console.log(response);
-        })
-        .catch(error => {
-            console.log(error);
-        });
+            .then(response => {
+                response.json()
+                console.log(response);
+            })
+            .catch(error => {
+                console.log(error);
+            });
     }
-    
 
+    deleteTrack = (playlist_id, track_id, position) => {
+        const url = `https://api.spotify.com/v1/playlists/${playlist_id}/tracks`
+        fetch(url, {
+            method: "DELETE",
+            headers: {
+                Authorization: "authorization/json",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                "tracks": [{ "uri": `spotify:track:${track_id}`, "positions": [position] }]
+            }),
+        })
+            .then(response => {
+                response.json()
+                console.log(response);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
 
 }
