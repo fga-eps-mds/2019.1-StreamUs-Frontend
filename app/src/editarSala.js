fb37a4b28e07182e5074af48b8434b7bdae82ab0
Nícalo Ribeiro <nicalo63@gmail.com>
2019-06-14 01:38:03 -0300

    fga-eps-mds/2019.1-StreamUs-Backend#51
    Some visual adjustments

--- app/src/editarSala.js
+++ app/src/editarSala.js
@@ -2,64 +2,93 @@ import React, { Component } from 'react';
 import { StyleSheet, View } from 'react-native'
 
 
-export default class EditRoom extends Component {
+export default class EditarSala extends Component {
 
-    state = { permission: '' }
+    state = {permission: ''}
     updatePermission = (permission) => {
-        this.setState({ permission: permission })
+       this.setState({ permission : permission })
     }
-
-    render() {
-        return (
-            <View style={stylesx.container}>
-                <View style={stylesx.cont2}>
-                    <Text style={styles.titulo} > EDITAR SALA  </Text>
-                    <Text style={styles.texto} >  Nome da sala </Text>
-                    <TextInput style={styles.textInput}
-                        placeholder='Digite o nome da sala'
-                        paddingLeft={5}>
-                    </TextInput>
-                    <Text style={styles.texto}> Permissão da sala </Text>
-                    <Picker
-                        selectedValue={this.state.permission}
-                        style={{ marginTop: 5, borderRadius: 20, height: 30, width: 150, backgroundColor: 'white', }}
-                        onValueChange={this.updatePermission}
-                    >
-                        <Picker.Item label="Pública" value="public" />
-                        <Picker.Item label="Privada" value="private" />
-                    </Picker>
-                </View>
-                <View style={stylesx.cont3}>
-                    <TouchableOpacity style={styles.botao}>
-                        <Text style={styles.textoBotao}> SALVAR </Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={styles.botao}>
-                        <Text style={styles.textoBotao}> VOLTAR </Text>
-                    </TouchableOpacity>
-                </View>
-
-            </View>
-        );
-    }
-}
-
-const stylesx = StyleSheet.create({
-    container: {
-        flex: 1,
-        //alignItens: 'center',
-        //justifyContent: 'center',
-        backgroundColor: '#191414',
-    },
-    cont2: {
-        marginTop: 70,
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItens: 'center',
-        backgroundColor: '#191414',
-    },
-    cont3: {
-        flexDirection: 'row',
-
-    },
-
-});
+   render() {
+     return (
+       <View style = {stylesx.container}>
+         <View  style = {stylesx.background}>
+         <View style = {stylesx.cont2}>
+           <Text style = {styles.titulo} > EDITAR SALA  </Text>     
+           <Text style = {styles.texto} >  Nome da sala </Text>
+           <TextInput style = {styles.textInput}
+             placeholder = 'Digite o nome da sala'
+             paddingLeft = {5}>       
+           </TextInput>
+           <Text style = {styles.texto}> Permissão da sala </Text>
+           <Picker        
+             selectedValue={this.state.permission}
+             style={{marginTop : 5, borderRadius: 20, width: 150, height : 30, backgroundColor : 'white',}}
+             onValueChange = {this.updatePermission}
+             >
+             <Picker.Item label="Pública" value="public" />
+             <Picker.Item label="Privada" value="private" />
+           </Picker>
+           <TouchableOpacity style = {stylesx.apagar}>
+           <Text style = {styles.textoBotao}> DELETAR SALA </Text>
+           </TouchableOpacity>
+         </View>
+         
+         <View style = {stylesx.cont3}>
+           <TouchableOpacity style = {styles.botao}>
+             <Text style = {styles.textoBotao}> SALVAR </Text>
+           </TouchableOpacity>
+           <TouchableOpacity style = {styles.botao}>
+             <Text style = {styles.textoBotao}> VOLTAR </Text>
+           </TouchableOpacity>
+         </View>
+          
+         </View>
+       </View>
+     );
+   }
+ }
+ 
+ const stylesx = StyleSheet.create({
+   container: {
+     flex: 1,
+     alignItens: 'center',
+     backgroundColor: '#191414',
+     
+   },
+   background : {
+     flex : 1,
+     backgroundColor : 'gray',  
+     marginLeft: 15,
+     marginRight : 15,
+     marginTop : 50,
+     marginBottom : 50,
+   },
+   cont2 : {        
+     marginTop: 50,
+     marginLeft: 15,
+     marginRight : 15,
+     justifyContent: 'center',
+     alignItens : 'center',
+         
+     
+   },
+   cont3: {
+     flexDirection : 'row',
+     justifyContent : 'center',
+     alignItens : 'center',
+     marginTop : 15,
+ 
+ 
+   },
+   apagar : {
+     
+     justifyContent: 'center',
+     backgroundColor: 'red',
+     marginTop : 10,
+     marginBottom : 10,
+     width: 250,
+     height: 40,
+     borderRadius: 20,
+     elevation: 5,
+   },
+ });
