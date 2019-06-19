26528a6233c8b0e3da15d67109c354e507cf308e
Nícalo Ribeiro <nicalo63@gmail.com>
2019-06-19 02:38:44 -0300

    fga-eps-mds/2019.1-StreamUs-Backend#47
    Add request.
    
    Co-authored-by: João Victor <joao.bisinotti@candev.com.br>
    Co-authored-by: pdaniel37 <pedrodaniel@candev.com.br>

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
-import {Button, Image, View, Text } from 'react-native';
-import { default as ArtistaMusica} from './ArtistaMusica';
-
-
+import { Button, Image, View, Text } from 'react-native';
+import { default as ArtistaMusica } from './ArtistaMusica';
 
 // const obj =[] objeto de teste
-const obj = [
-  {
+const axios = require('axios')
+let obj = [{
     artista: "Lucas",
     musica: "My songL"
   },
@@ -17,35 +15,60 @@ const obj = [
   {
     artista: "Ygor",
     musica: "My songY"
-  }
-]
+}
+  
+];
 
+const url = 'https://api.spotify.com/v1/search?q=despacito&type=track&limit=10'
+const accessToken = 'BQADUOZpvx4S1pFkF9BjqvQ3SPjqgRMXREjCm1DbHPqmz25Q2f7lHwy680apfIgFkjWHbJqM6YMR2XAJEAVHvtIQO5JXZ7M-jW0syb23Pw6_J97Pb3D5d_biSFN8M-JMTuYCxNfWaJ41MCqmMGxe4UvWwdQZ-LtcvmiMDTnzdgS50Z8pDAF8OcZ7cj-NjZabaeFmDobyeIoLeBLjplo8uDFI8Sj1hzCCGv7qkCfRbmWQBC60XpzazYjUh86bFh-4mIjN1F-qKsLTQmhTYQYB'
 
-const urlImg = 'https://facebook.github.io/react-native/docs/assets/favicon.png'
+const urlImg =
+  'https://facebook.github.io/react-native/docs/assets/favicon.png';
 
 export default class FormExample extends Component {
   render() {
-    let res = [];
+    let resp = [];
+     //let res = [];
+    
+fetch(url, {
+            method: "GET",
+            headers: {
+                'Authorization': 'Bearer ' + accessToken,
+		Accept: "application/json",
+          	"Content-Type": "application/json",
+            }
+        })
+        .then(response => {
+          response.json()
+          console.log(response)
+	 obj = response.tracks.items
+	 alert(response)
+        })
+        .catch(error => {
+          console.log(error)
+        });
 
     //tamanho do resultado da busca
-    let SIZE = obj.length
+    let SIZE = obj.length;
     for (let i = 0; i < SIZE; i++) {
-      res.push(
+      resp.push(
         <View key={i}>
-          <ArtistaMusica artista={obj[i].artista} musica={obj[i].musica}  urlImg ={urlImg} />
+          <ArtistaMusica
+            artista={obj[i].artista}
+            musica={obj[i].musica}
+            urlImg={urlImg}
+          />
         </View>
-      )
+      );
     }
     return (
-
       <View>
-        <Text>
-          Musica{"\n\n"} 
-        </Text>
-
-        {res}
+        <Text>Musica{'\n\n'}</Text>
+        {resp}
+       
       </View>
-
+      
     );
+    
   }
 }
