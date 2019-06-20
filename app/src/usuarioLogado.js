66375574f9f29fc5e52773b731ae43c70b63eae6
Nícalo Ribeiro <nicalo63@gmail.com>
2019-06-20 12:31:12 -0300

    fga-eps-mds/2019.1-StreamUs-Backend#39
    Adjusting layout.
    
    Co-authored-by: oliveiraMarcelo <martins.oliveira.mo@gmail.com>

--- app/src/usuarioLogado.js
+++ app/src/usuarioLogado.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Text, View, StyleSheet, StatusBar, ScrollView , Image} from 'react-native';
+import { Text, View, StyleSheet, StatusBar, ScrollView, Image, TouchableOpacity } from 'react-native';
 import Constants from 'expo-constants';
 
 // You can import from local files
@@ -10,117 +10,157 @@ import { Card } from 'react-native-paper';
 
 
 export default class App extends React.Component {
-  render() {
-    const user = {
-  "display_name":"JMWizzler",
-  "email":"email@example.com",
-  "external_urls":{
-  "spotify":"https://open.spotify.com/user/wizzler"
-  },
-  "href":"https://api.spotify.com/v1/users/wizzler",
-  "id":"wizzler",
-  "images":{
-  "height":null,
-  "url": 'https://facebook.github.io/react-native/docs/assets/favicon.png',
-  "width":null
-  },
-  "product":"premium",
-  "type":"user",
-  "uri":"spotify:user:wizzler"
-}
+    render() {
+        const user = {
+            "display_name": "JMWizzler",
+            "email": "email@example.com",
+            "external_urls": {
+                "spotify": "https://open.spotify.com/user/wizzler"
+            },
+            "href": "https://api.spotify.com/v1/users/wizzler",
+            "id": "wizzler",
+            "images": [{
+                "height": null,
+                "url": 'https://facebook.github.io/react-native/docs/assets/favicon.png',
+                "width": null
+            }],
+            "product": "premium",
+            "type": "user",
+            "uri": "spotify:user:wizzler"
+        }
 
-    return (
+        return (
 
-      <View style = {styles.mainView}>
-        <StatusBar hidden = {true}/>
+            <View style={styles.mainView}>
+                <StatusBar hidden={true} />
 
-        <View style = {styles.toolBar}>
-        </View>
-        
-        <View style = {{flex :1, backgroundColor :'red'}}> 
-        
-        
-          <View style = {{  flexDirection : 'row', backgroundColor : 'white', height : 100}}>
-            <View style = {styles.userImageView}>
-              <Image style={styles.userProfileImage} source={{ uri: user.images.url}} />
+                <View style={styles.toolBar}>
+                    <Text style={{ fontSize: 30, color: 'white', fontWeight: 'bold', textAlign: 'center' }}> Perfil </Text>
+                </View>
+
+                <View style={{ flex: 1 }}>
+
+
+                    <View style={{ flexDirection: 'row', backgroundColor: 'white', height: 100 }}>
+                        <View style={styles.userImageView}>
+                            <Image style={styles.userProfileImage} source={{ uri: user.images[0].url }} />
+                        </View>
+
+                        <View style={styles.userInfoView}>
+                            <Text style={styles.userInfoText}>{user.display_name}</Text>
+                            <Text style={styles.userInfoText}>{user.id}</Text>
+                        </View>
+
+                    </View>
+
+
+                    <View style={{ alignItems: 'center' }}>
+                        <TouchableOpacity style={styles.botao}>
+                            <Text style={styles.textoBotao}> Minhas Salas </Text>
+                        </TouchableOpacity>
+
+                        <TouchableOpacity style={styles.botao}>
+                            <Text style={styles.textoBotao}> Criar Sala </Text>
+                        </TouchableOpacity>
+
+                        <TouchableOpacity style={styles.botao}>
+                            <Text style={styles.textoBotao}> Entrar em sala </Text>
+                        </TouchableOpacity>
+                    </View>
+
+                </View>
+
+                <View style={styles.footer}>
+
+                </View>
+
             </View>
-          
-            <View style = {styles.userInfoView}>
-              <Text style = {styles.userInfoText}>{user.display_name}</Text>
-                <Text style = {styles.userInfoText}>{user.email}</Text>
-              <Text style = {styles.userInfoText}>{user.id}</Text>
-            </View>
 
-          </View>
-       
+        );
 
-        </View>
-
-        <View style = {styles.footer}>
-        
-        </View>
-
-      </View>
-     
-    );
-
-  }
+    }
 }
 
 const styles = StyleSheet.create({
-  mainView : {
-    flex : 1,
-    backgroundColor : 'black'
-  },
+    mainView: {
+        flex: 1,
+        backgroundColor: 'black'
+    },
     toolBar: {
-  
-      flexDirection: 'row',
-      height: 50,
-      marginBottom: 10,
-      backgroundColor: '#191414',
-      justifyContent: 'center',
-      alignItens: 'center',
-  
-    },
-  
 
-      footer: {
-      
-      height: 50,
-      marginTop: 10,
-      backgroundColor: '#191414',
-      justifyContent: 'center',
-      alignItens: 'center',
-  
-    },
-
-    userImageView : {
-      flexDirection : 'column',
-      height :100,
-      width : 100,
-      backgroundColor : 'blue',
-    },
-
-    userInfoView : {
-      flex: 1,
-      flexDirection : 'column',
-      backgroundColor : 'green',
-      margin : 5
-    },
-
-    userInfoText : {
-      marginLeft: 10,
-      marginTop : 5,
-      color : 'white',
-      fontSize : 15
+        flexDirection: 'row',
+        height: 50,
+        marginBottom: 10,
+        backgroundColor: '#191414',
+        justifyContent: 'center',
+        alignItens: 'center',
 
     },
 
-    userProfileImage : {
-      width: 80,
-      height: 80,
-      margin : 10
+
+    footer: {
+
+        height: 50,
+        marginTop: 10,
+        backgroundColor: '#191414',
+        justifyContent: 'center',
+        alignItens: 'center',
 
     },
+
+    userImageView: {
+        flexDirection: 'column',
+        height: 100,
+        width: 100,
+        backgroundColor: 'white',
+    },
+
+    userInfoView: {
+        flex: 1,
+        flexDirection: 'column',
+        backgroundColor: 'black',
+        margin: 5
+    },
+
+    userInfoText: {
+        marginLeft: 10,
+        marginTop: 5,
+        color: 'white',
+        fontSize: 15
+
+    },
+
+    userProfileImage: {
+        width: 90,
+        height: 90,
+        margin: 5
+
+    },
+
+    botao: {
+        alignItens: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#1DB954',
+        borderColor: 'white',
+        width: 250,
+        height: 40,
+        marginTop: 25,
+        borderRadius: 20,
+        borderWidth: 0.5,
+
+
+
+
+    },
+
+    textoBotao: {
+
+        textAlign: 'center',
+        fontSize: 15,
+        color: '#FFFFFF',
+        fontWeight: 'bold',
+
+    },
+
 
 });
