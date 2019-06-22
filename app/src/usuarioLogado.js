623310bc912faf1696226bde54769c7bb96b96a2
Nícalo Ribeiro <nicalo63@gmail.com>
2019-06-21 21:45:16 -0300

    fga-eps-mds/2019.1-StreamUs-Backend#39
    Getting user info from request.
    
    Co-authored-by: iagoomr <iagorag363@gmail.com>
    Co-authored-by: LucasPLopes <l.lopes.fga@gmail.com>
    Co-authored-by: Matheus Clemente <matheus_klementt@hotmail.com>

--- app/src/usuarioLogado.js
+++ app/src/usuarioLogado.js
@@ -1,177 +1,189 @@
 import * as React from 'react';
-import { Text, View, StyleSheet, StatusBar, ScrollView , Image, TouchableOpacity} from 'react-native';
-import Constants from 'expo-constants';
+import { Text, View, StyleSheet, StatusBar, ScrollView, Image, TouchableOpacity, AsyncStorage } from 'react-native';
 
-// You can import from local files
-import AssetExample from './components/AssetExample';
-
-// or any pure javascript modules available in npm
-import { Card } from 'react-native-paper';
 
 const axios = require('axios')
-const acess_token = 'BQDEQRQXdpvwMNHLigeCu0c8fkQicOGXxGTAoiFrtjLji_caBsFZ2'
+const acess_token = 'BQAwrykr5xtpMxYKf1lDtCkK96ButJc562Ee9bKDV-kIcrEXQJI9PNqZEG_ZNInstqOfu-nywDbVZuSfViGUih2NjI9eifw4m0C3OaoJAHZoaw5YpCD6n6hah1N3B_z-dBsJ9yxss8Js89MdpM70wRAk_lipVsA_U5TIf1r4dJY7uohgRopzsSTVtrW7QULjHdgMpKAB7W_fXvNZJRbSNPyxW6nZ1wWoK_fnLeynUyOWc7BxWUth9JE6uc2T9eROFD5vfHbznQ5_AnIkZVQM'
+
 const user_id = '12154583081'
+
 export default class App extends React.Component {
-   async getPlaylistTrack (user_id) {
+  state = {
+    usuario: {}
+  };
+
+  componentDidMount() {
+    this.getPlaylistTrack(user_id);
+  }
+
+  async getPlaylistTrack(user_id) {
     if (user_id) {
-      try { 
-      const {data} = await axios.get(`https://api.spotify.com/v1/users/${user_id}`, {headers: {Authorization: 'Bearer ' + acess_token}})
-      console.log(data);
-      
-      alert(data)
-      return data;
+      try {
+        let { data } = await axios.get(`https://api.spotify.com/v1/users/${user_id}`, { headers: { Authorization: 'Bearer ' + acess_token } })
+
+
+        this.setState({ usuario: data })
+        return data
+
       }
-      catch(err) {
+      catch (err) {
         throw new Error(err)
       }
-      
+
     }
-   }
+  }
+
   render() {
 
- const data = this.getPlaylistTrack(user_id)
+
+    const data1 = this.getPlaylistTrack(user_id)
+
 
     const user = {
-  "display_name":"JMWizzler",
-  "email":"email@example.com",
-  "external_urls":{
-  "spotify":"https://open.spotify.com/user/wizzler"
-  },
-  "href":"https://api.spotify.com/v1/users/wizzler",
-  "id":"wizzler",
-  "images":[{
-  "height":null,
-  "url": 'https://facebook.github.io/react-native/docs/assets/favicon.png',
-  "width":null
-  }],
-  "product":"premium",
-  "type":"user",
-  "uri":"spotify:user:wizzler"
-}
+      "display_name": "JMWizzler",
+      "email": "email@example.com",
+      "external_urls": {
+        "spotify": "https://open.spotify.com/user/wizzler"
+      },
+      "href": "https://api.spotify.com/v1/users/wizzler",
+      "id": "wizzler",
+      "images": [{
+        "height": null,
+        "url": 'https://facebook.github.io/react-native/docs/assets/favicon.png',
+        "width": null
+      }],
+      "product": "premium",
+      "type": "user",
+      "uri": "spotify:user:wizzler"
+    }
 
     return (
 
-      <View style = {styles.mainView}>
-        <StatusBar hidden = {true}/>
+      <View style={styles.mainView}>
+        <StatusBar hidden={true} />
 
-        <View style = {styles.toolBar}>
-          <Text style = {{fontSize : 30, color : 'white', fontWeight : 'bold', textAlign : 'center'}}> Perfil </Text>
+        <View style={styles.toolBar}>
+          <Text style={{ fontSize: 30, color: 'white', fontWeight: 'bold', textAlign: 'center' }}> Perfil </Text>
         </View>
-        
-        <View style = {{flex :1}}> 
-        
-        
-          <View style = {{  flexDirection : 'row', backgroundColor : 'white', height : 100}}>
-            
-          
-            <View style = {styles.userInfoView}>
-              <Text style = {styles.userInfoText}>{data.display_name}</Text>
-              <Text style = {styles.userInfoText}>{data.id}</Text>
+
+        <View style={{ flex: 1 }}>
+
+
+          <View style={{ flexDirection: 'row', backgroundColor: 'white', height: 100 }}>
+
+            <View style={styles.userImageView}>
+              <Image style={styles.userProfileImage}
+                source={{ uri: this.state.usuario.images[0].url }} />
+            </View>
+
+            <View style={styles.userInfoView}>
+              <Text style={styles.userInfoText}>{this.state.usuario.display_name}</Text>
+              <Text style={styles.userInfoText}>{this.state.usuario.id}</Text>
             </View>
 
           </View>
 
 
-        <View style = {{alignItems : 'center'}}>
-          <TouchableOpacity style = {styles.botao}>
-            <Text style = {styles.textoBotao}> Minhas Salas </Text>
-          </TouchableOpacity>
+          <View style={{ alignItems: 'center' }}>
+            <TouchableOpacity style={styles.botao}>
+              <Text style={styles.textoBotao}> Minhas Salas </Text>
+            </TouchableOpacity>
 
-          <TouchableOpacity style = {styles.botao}>
-            <Text style = {styles.textoBotao}> Criar Sala </Text>
-          </TouchableOpacity>
+            <TouchableOpacity style={styles.botao}>
+              <Text style={styles.textoBotao}> Criar Sala </Text>
+            </TouchableOpacity>
 
-          <TouchableOpacity style = {styles.botao}>
-            <Text style = {styles.textoBotao}> Entrar em sala </Text>
-          </TouchableOpacity>
-       </View>
+            <TouchableOpacity style={styles.botao}>
+              <Text style={styles.textoBotao}> Entrar em sala </Text>
+            </TouchableOpacity>
+          </View>
 
         </View>
 
-        <View style = {styles.footer}>
-        
+        <View style={styles.footer}>
+
         </View>
 
       </View>
-     
-    )
-    }
 
+    )
   }
 
+}
+
 const styles = StyleSheet.create({
-  mainView : {
-    flex : 1,
-    backgroundColor : 'black'
+  mainView: {
+    flex: 1,
+    backgroundColor: 'black'
   },
-    toolBar: {
-  
-      flexDirection: 'row',
-      height: 50,
-      marginBottom: 10,
-      backgroundColor: '#191414',
-      justifyContent: 'center',
-      alignItens: 'center',
-  
-    },
-  
+  toolBar: {
 
-      footer: {
-      
-      height: 50,
-      marginTop: 10,
-      backgroundColor: '#191414',
-      justifyContent: 'center',
-      alignItens: 'center',
-  
-    },
+    flexDirection: 'row',
+    height: 50,
+    marginBottom: 10,
+    backgroundColor: '#191414',
+    justifyContent: 'center',
+    alignItens: 'center',
 
-    userImageView : {
-      flexDirection : 'column',
-      height : 100,
-      width : 100,
-      backgroundColor : 'white',
-    },
+  },
 
-    userInfoView : {
-      flex: 1,
-      flexDirection : 'column',
-      backgroundColor : 'black',
-      margin : 5
-    },
 
-    userInfoText : {
-      marginLeft: 10,
-      marginTop : 5,
-      color : 'white',
-      fontSize : 15
+  footer: {
 
-    },
+    height: 50,
+    marginTop: 10,
+    backgroundColor: '#191414',
+    justifyContent: 'center',
+    alignItens: 'center',
 
-    userProfileImage : {
-      width: 90,
-      height: 90,
-      margin : 5
+  },
 
-    },
+  userImageView: {
+    flexDirection: 'column',
+    height: 100,
+    width: 100,
+    backgroundColor: 'white',
+  },
 
-    botao: {
+  userInfoView: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'black',
+    margin: 5
+  },
+
+  userInfoText: {
+    marginLeft: 10,
+    marginTop: 5,
+    color: 'white',
+    fontSize: 15
+
+  },
+
+  userProfileImage: {
+    width: 90,
+    height: 90,
+    margin: 5
+
+  },
+
+  botao: {
     alignItens: 'center',
     justifyContent: 'center',
     backgroundColor: '#1DB954',
-    borderColor : 'white',
+    borderColor: 'white',
     width: 250,
     height: 40,
-    marginTop : 25,
+    marginTop: 25,
     borderRadius: 20,
     borderWidth: 0.5,
-   
-   
+
+
 
 
   },
 
-   textoBotao: {
+  textoBotao: {
 
     textAlign: 'center',
     fontSize: 15,
