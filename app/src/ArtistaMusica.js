05e51d891631102a8ad775b89a670dda2136d145
Matheus Clemente <matheus_klementt@hotmail.com>
2019-06-21 22:12:15 -0300

    fga-eps-mds/2019.1-StreamUs-Backend#53- Showing complete playlist tracks

--- app/src/ArtistaMusica.js
+++ app/src/ArtistaMusica.js
@@ -1,34 +1,94 @@
 import React, { Component } from 'react';
-import {StyleSheet,TouchableOpacity, Image, View, Text } from 'react-native';
+import { StyleSheet, TouchableOpacity, Image, View, Text } from 'react-native';
 
 export default class ArtistaMusica extends Component {
-    render() {
-      return (
-        <View >
+  render() {
+    return (
+      <View>
         <View style={styles.align}>
-          <View>
-            <Image style={{width: 50, height: 50}} source={{uri: this.props.urlImg}} />
+          <View style={{ marginLeft: 10 }}>
+            <Image style={{ width: 50, height: 50 }} source={{ uri: this.props.urlImg }} />
           </View>
-          <View>
-            <Text>  Artista: {this.props.artista}</Text>
-            <Text>  Música: {this.props.musica}</Text>
+
+          <View style={styles.viewItem}>
+
+            <Text style={styles.texto}>  Música: {this.props.musica}</Text>
+            <Text style={styles.texto}>  Artista: {this.props.artista}</Text>
+
           </View>
+
+          <View style={styles.viewBotao}>
+            <TouchableOpacity style={styles.botao} >
+              <Text style={styles.textoBotao}>
+                Adicionar
+            </Text>
+            </TouchableOpacity >
+          </View>
+
         </View>
         <View >
-          <TouchableOpacity >
-            <Text>
-              Adicionar
-            </Text>
-          </TouchableOpacity >
-        </View>  
+
         </View>
-      );
-    }
+      </View>
+    );
   }
+}
 
 
 const styles = StyleSheet.create({
+
   align: {
-    flexDirection: 'row'
-  }
-  });
+    flexDirection: 'row',
+    height: 70,
+    alignItems: 'center',
+    borderWidth: 0.5,
+    borderBottomColor: 'white',
+    marginBottom: 1,
+
+  },
+  texto: {
+
+    fontSize: 15,
+    textAlign: 'left',
+    color: 'black',
+    fontWeight: 'bold',
+
+  },
+
+  botao: {
+
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderColor: 'white',
+    width: 100,
+    height: 30,
+    borderRadius: 20,
+    marginLeft: 5,
+    marginRight: 5,
+    borderWidth: 0.5,
+
+
+  },
+
+  textoBotao: {
+
+    textAlign: 'center',
+    fontSize: 15,
+    color: '#FFFFFF',
+    fontWeight: 'bold',
+
+  },
+
+  viewBotao: {
+
+    marginLeft: 35
+
+  },
+
+  viewItem: {
+    marginLeft: 10,
+    width: 200
+
+  },
+
+});
--- app/src/ShowPlaylist.js
+++ app/src/ShowPlaylist.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
 import ArtistaMusica from './ArtistaMusica'
-import {View, Text} from 'react-native';
+import {View, ScrollView, Text, StyleSheet} from 'react-native';
 import axios from './axiosintercept'
 
 export default class ShowPlaylist extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      items: [],
+    };
+  }
+
     async fetchMusic() {
-      const playlist_id = '' //id da playlist a ser mostrada
+      const playlist_id = ''
       try { 
         const {data} = await axios.get(`https://api.spotify.com/v1/playlists/${playlist_id}/tracks`)
-        alert(JSON.stringify(data.items))
-        return data;
+        //alert(JSON.stringify(data))
+        this.setState({items: data.items})
         }
         catch(err) {
           throw new Error(err)
@@ -17,14 +25,50 @@ export default class ShowPlaylist extends Component {
       
     }
     componentDidMount() {
-        this.fetchMusic()
+      this.fetchMusic()
     }
     render() {
+      let resp = [];
+
+      //alert(JSON.stringify(this.state.items))
+
+      //tamanho da playlist
+      let SIZE = this.state.items.length;
+
+      for (let i = 0; i < SIZE; i++) 
+        resp.push(
+          <View key={i}>
+            <ArtistaMusica
+              artista={this.state.items[i].track.artists[0].name}
+              musica={this.state.items[i].track.name}
+              urlImg={this.state.items[i].track.album.images[0].url}
+            />
+          </View>
+        );
+
         return (
-            <View>        
-              <ArtistaMusica artista='artista' musica='musica'/>
-              <Text onClick={() => this.fetchMusic()}>aloo</Text>
-            </View>
+         // <View style={styles.mainView}>
+            <ScrollView>
+              <View style= {StyleSheet.showMusics}>        
+                {resp}
+              </View>
+            </ScrollView>
+         // </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+  mainView: {
+    flex: 1,
+    backgroundColor: 'black',
+    justifyContent: 'center',
+  },
+
+  showMusics: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+    marginTop: 10,
+  },
+});
