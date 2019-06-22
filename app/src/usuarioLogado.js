065b5e8c636e311ffd4925e64a986f831f45b58e
Lucas Lopes Pereira <44032349+LucasPLopes@users.noreply.github.com>
2019-06-22 00:02:35 -0300

    fga-eps-mds/2019.1-StreamUs-Backend#39
     Finishing profile request and adjusting style 
    
    Co-authored-by: iagoomr <iagorag363@gmail.com>
    Co-authored-by: Nícalo Ribeiro <nicalo63@gmail.com>

--- app/src/usuarioLogado.js
+++ app/src/usuarioLogado.js
@@ -1,195 +1,199 @@
 import * as React from 'react';
-import { Text, View, StyleSheet, StatusBar, ScrollView, Image, TouchableOpacity, AsyncStorage } from 'react-native';
+import { Text, View, StyleSheet, StatusBar, ScrollView , Image, TouchableOpacity, AsyncStorage} from 'react-native';
 
 
 const axios = require('axios')
-const acess_token = 'BQAwrykr5xtpMxYKf1lDtCkK96ButJc562Ee9bKDV-kIcrEXQJI9PNqZEG_ZNInstqOfu-nywDbVZuSfViGUih2NjI9eifw4m0C3OaoJAHZoaw5YpCD6n6hah1N3B_z-dBsJ9yxss8Js89MdpM70wRAk_lipVsA_U5TIf1r4dJY7uohgRopzsSTVtrW7QULjHdgMpKAB7W_fXvNZJRbSNPyxW6nZ1wWoK_fnLeynUyOWc7BxWUth9JE6uc2T9eROFD5vfHbznQ5_AnIkZVQM'
+const acess_token = 'BQA2c0pcH44_9y-1FndaEdgG0UpLtpVGBzrgmyxduCMTBy9wxm6PsK7lB5TAkMaJ1FqZ8FcQ3AWOanmpKjbLzk8BezjOTpRrR2phUBJ7kJSSU4qIjQrxGHmyBG-QmbgSotHW2gWnuKazzttk2rLH8n0O8YNYKBNoyJlUV_spOD9MTYLQ7R9nRfFvphVI9dJ8a7ef8NmfBPvSE2r0G5SXVHpETb7S1AjyiHiIp6kGYosIVv5FaHWILyAMgGqAXK5k_4uROb9lfHZf4myHdCeB'
 
 const user_id = '12154583081'
 
-export default class App extends React.Component {
+export default class UsuarioLogado extends React.Component {
   state = {
-    usuario: {}
-  };
+       usuario: {},
+       image:{}
+};
 
-  componentDidMount() {
-    this.getPlaylistTrack(user_id);
-  }
+componentDidMount() {
+       this.getPlaylistTrack(user_id);
+}
 
-  async getPlaylistTrack(user_id) {
+   async getPlaylistTrack (user_id) {
     if (user_id) {
-      try {
-        let { data } = await axios.get(`https://api.spotify.com/v1/users/${user_id}`, { headers: { Authorization: 'Bearer ' + acess_token } })
-
-
-        this.setState({ usuario: data })
-        return data
-
+      try { 
+    let {data} = await axios.get(`https://api.spotify.com/v1/users/${user_id}`, {headers: {Authorization: 'Bearer ' + acess_token}})    
+      
+      this.setState({ usuario: data, image: data.images[0].url })     
+      
       }
-      catch (err) {
+      catch(err) {
         throw new Error(err)
       }
-
+      
     }
-  }
+   }
 
   render() {
 
-
-    const data1 = this.getPlaylistTrack(user_id)
-
+ 
+  let getData =  this.getPlaylistTrack(user_id)
+  console.log(this.state.usuario.images)
 
     const user = {
-      "display_name": "JMWizzler",
-      "email": "email@example.com",
-      "external_urls": {
-        "spotify": "https://open.spotify.com/user/wizzler"
-      },
-      "href": "https://api.spotify.com/v1/users/wizzler",
-      "id": "wizzler",
-      "images": [{
-        "height": null,
-        "url": 'https://facebook.github.io/react-native/docs/assets/favicon.png',
-        "width": null
-      }],
-      "product": "premium",
-      "type": "user",
-      "uri": "spotify:user:wizzler"
-    }
+  "display_name":"JMWizzler",
+  "email":"email@example.com",
+  "external_urls":{
+  "spotify":"https://open.spotify.com/user/wizzler"
+  },
+  "href":"https://api.spotify.com/v1/users/wizzler",
+  "id":"wizzler",
+  "images":[{
+  "height":null,
+  "url": 'https://facebook.github.io/react-native/docs/assets/favicon.png',
+  "width":null
+  }],
+  "product":"premium",
+  "type":"user",
+  "uri":"spotify:user:wizzler"
+}
 
     return (
 
-      <View style={styles.mainView}>
-        <StatusBar hidden={true} />
+      <View style = {styles.mainView}>
+        <StatusBar hidden = {true}/>
 
-        <View style={styles.toolBar}>
-          <Text style={{ fontSize: 30, color: 'white', fontWeight: 'bold', textAlign: 'center' }}> Perfil </Text>
+        <View style = {styles.toolBar}>
+          <Text style = {{fontSize : 30, marginTop: 5, color : 'white', fontWeight : 'bold', textAlign : 'center'}}> Perfil </Text>
         </View>
-
-        <View style={{ flex: 1 }}>
-
-
-          <View style={{ flexDirection: 'row', backgroundColor: 'white', height: 100 }}>
-
-            <View style={styles.userImageView}>
-              <Image style={styles.userProfileImage}
-                source={{ uri: this.state.usuario.images[0].url }} />
+        
+        <View style = {{flex :1}}> 
+        
+        
+          <View style = {styles.perfilBorda}>
+            
+            <View style = {styles.userImageView}>
+             <Image style = {styles.userProfileImage} 
+             source={{ uri: `${this.state.image}` }}
+             />
             </View>
-
-            <View style={styles.userInfoView}>
-              <Text style={styles.userInfoText}>{this.state.usuario.display_name}</Text>
-              <Text style={styles.userInfoText}>{this.state.usuario.id}</Text>
+            
+            <View style = {styles.userInfoView}>
+              <Text style = {styles.userInfoText}>{this.state.usuario.display_name}</Text>
             </View>
 
           </View>
 
 
-          <View style={{ alignItems: 'center' }}>
-            <TouchableOpacity style={styles.botao}>
-              <Text style={styles.textoBotao}> Minhas Salas </Text>
-            </TouchableOpacity>
+        <View style = {{alignItems : 'center'}}>
+          <TouchableOpacity style = {styles.botao}>
+            <Text style = {styles.textoBotao}> Minhas Salas </Text>
+          </TouchableOpacity>
 
-            <TouchableOpacity style={styles.botao}>
-              <Text style={styles.textoBotao}> Criar Sala </Text>
-            </TouchableOpacity>
+          <TouchableOpacity style = {styles.botao}>
+            <Text style = {styles.textoBotao}> Criar Sala </Text>
+          </TouchableOpacity>
 
-            <TouchableOpacity style={styles.botao}>
-              <Text style={styles.textoBotao}> Entrar em sala </Text>
-            </TouchableOpacity>
-          </View>
+          <TouchableOpacity style = {styles.botao}>
+            <Text style = {styles.textoBotao}> Entrar em sala </Text>
+          </TouchableOpacity>
+       </View>
 
         </View>
 
-        <View style={styles.footer}>
-
+        <View style = {styles.footer}>
+          <Text style ={styles.footerText}>StreamUs</Text>
         </View>
 
       </View>
-
+     
     )
+    }
+
   }
 
-}
-
 const styles = StyleSheet.create({
-  mainView: {
-    flex: 1,
-    backgroundColor: 'black'
+  mainView : {
+    flex : 1,
+    backgroundColor : 'black'
   },
-  toolBar: {
+    toolBar: {
+  
+      flexDirection: 'row',
+      height: 50,
+      marginBottom: 10,
+      backgroundColor: '#191414',
+      justifyContent: 'center',
+      alignItens: 'center',
+     
+    },
+    perfilBorda:{ 
+             flexDirection : 'row',
+             marginLeft:25 ,
+             marginRight:25,
+             height : 115,              
+             },
+  
 
-    flexDirection: 'row',
-    height: 50,
-    marginBottom: 10,
-    backgroundColor: '#191414',
-    justifyContent: 'center',
-    alignItens: 'center',
+      footer: {
+      
+      height: 50,
+      marginTop: 10,
+      backgroundColor: '#191414',
+      justifyContent: 'center',
+      alignItens: 'center',
+  
+    }, footerText: 
+    {      fontSize: 15,     color: 'white',     fontWeight: 'bold',     textAlign: 'center'    },
 
-  },
+    userImageView : {
+      flexDirection : 'column',
+      height : 100,
+      width : 100,
+      // backgroundColor : 'black',
+    },
 
+    userInfoView : {
+      flex: 1,
+      flexDirection : 'column',
+      backgroundColor : 'black',
+      margin : 5
+    },
 
-  footer: {
+    userInfoText : {
+      marginLeft: 10,
+      marginTop : 5,
+      color : 'white',
+      fontSize : 25,
+      textAlign:'center',
+      fontWeight: 'bold',
 
-    height: 50,
-    marginTop: 10,
-    backgroundColor: '#191414',
-    justifyContent: 'center',
-    alignItens: 'center',
+    },
 
-  },
+    userProfileImage : {
+      width: 100,
+      height: 100,
+      margin : 5,
+      borderRadius:50,
 
-  userImageView: {
-    flexDirection: 'column',
-    height: 100,
-    width: 100,
-    backgroundColor: 'white',
-  },
+    },
 
-  userInfoView: {
-    flex: 1,
-    flexDirection: 'column',
-    backgroundColor: 'black',
-    margin: 5
-  },
-
-  userInfoText: {
-    marginLeft: 10,
-    marginTop: 5,
-    color: 'white',
-    fontSize: 15
-
-  },
-
-  userProfileImage: {
-    width: 90,
-    height: 90,
-    margin: 5
-
-  },
-
-  botao: {
+    botao: {
     alignItens: 'center',
     justifyContent: 'center',
     backgroundColor: '#1DB954',
-    borderColor: 'white',
+    borderColor : 'white',
     width: 250,
-    height: 40,
-    marginTop: 25,
-    borderRadius: 20,
+    height: 50,
+    marginTop : 25,
+    borderRadius: 25,
     borderWidth: 0.5,
+     },
 
-
-
-
-  },
-
-  textoBotao: {
+   textoBotao: {
 
     textAlign: 'center',
-    fontSize: 15,
+    fontSize: 20,
     color: '#FFFFFF',
     fontWeight: 'bold',
-
   },
 
 
