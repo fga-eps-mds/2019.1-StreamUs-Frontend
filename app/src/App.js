b6c4cbba368ff6302974731a8501bdc83adb0db9
Lucas Lopes Pereira <44032349+LucasPLopes@users.noreply.github.com>
2019-06-22 23:48:09 -0300

    Update App.js

--- app/src/App.js
+++ app/src/App.js
@@ -1,17 +1,40 @@
-import React, { Component } from 'react';
-import { Container, Header, Content, Form, Item, Input } from 'native-base';
-import CreatePlaylist from './CreatePaylist'
-import { AddTrack, DeleteTrack, ReorderTrack } from './ManagePlaylist'
-
-export default class App extends Component {
+import  React from 'react';
+import {
+  ScrollView,
+  Image,
+  StatusBar,
+  Text,
+  View,
+  StyleSheet
+} from 'react-native';
+import User from './home/user';
+import styles from './home/usuarioLogadoStyle';
+export default class App extends React.Component {
+  state = {
+    nomePlaylist: 'Massacration',
+    qtnUsers: 11,
+    users: [],
+  };
   render() {
+    let nome = 'Pikachu';
+    let urlimg =
+      'http://cdn.shopify.com/s/files/1/0257/6087/products/Pikachu_Single_Front_dc998741-c845-43a8-91c9-c1c97bec17a4.png?v=1523938908';
+
+
     return (
-      <div>
-        <CreatePlaylist />
-        <AddTrack playlist_id='' track_id=''/>
-        <DeleteTrack playlist_id='' track_id='' position={0} />
-        <ReorderTrack playlist_id='' start_position={0} end_position={0}/>
-      </div>
+      <View style= {{flex:1}}>
+        <StatusBar hidden={true} />
+
+        <Text>
+          {`${this.state.nomePlaylist}\nMembros:\t\t${this.state.qtnUsers}
+            `}
+        </Text>
+          <User nome={nome} foto={urlimg} />
+    
+        <View style={styles.footer}>
+          <Text style={styles.footerText}>StreamUs</Text>
+        </View>
+    </View>
     );
   }
 }
