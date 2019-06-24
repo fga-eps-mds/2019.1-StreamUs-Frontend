96f32f4f498bc74cb49d16b63483ed0748dc6025
Lucas Lopes Pereira <44032349+LucasPLopes@users.noreply.github.com>
2019-06-24 00:17:53 -0300

    Create App.js
    sample for demonstration  use of component user.js.

---
+++ app/src/home/App.js
@@ -0,0 +1,40 @@
+import  React from 'react';
+import {
+  ScrollView,
+  Image,
+  StatusBar,
+  Text,
+  View,
+  StyleSheet,Footer
+} from 'react-native';
+import User from './user';
+import styles from './usuarioLogadoStyle';
+export default class App extends React.Component {
+  state = {
+    nomePlaylist: 'Massacration',
+    qtnUsers: 11,
+    users: [],
+  };
+  render() {
+    let nome = 'Pikachu';
+    let urlimg =
+      'http://cdn.shopify.com/s/files/1/0257/6087/products/Pikachu_Single_Front_dc998741-c845-43a8-91c9-c1c97bec17a4.png?v=1523938908';
+
+
+    return (
+      <View style= {{flex:1}}>
+        <StatusBar hidden={true} />
+
+        <Text>
+          {`${this.state.nomePlaylist}\nMembros:\t\t${this.state.qtnUsers}
+            `}
+        </Text>
+          <User nome={nome} foto={urlimg} />
+    
+        <View style={styles.footer}>
+          <Text style={styles.footerText}>StreamUs</Text>
+        </View>
+    </View>
+    );
+  }
+}
